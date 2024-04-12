37d6c91a8038f188b8af0753876800e5cca324f4
kendall-dh <k.hendrix@ufl.edu>
2024-04-12 13:15:41 -0400

    Kendall's commit
    It finally works!

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# this is kendall's main file
+if __name__ == '__main__':
+def display_menu():
+    print("\nMenu\n"
+          "------------- \n")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+
+
+
+while True:
+    display_menu()
+    option = input("Please enter an option: ")
+
+    if option == "1":
+        choice = input("Please enter the password to encode: ")
+        update = 3
+        encoded_number = ""
+        for each_num in choice:
+            updated_num = str(int(each_num) + update % 10)
+            encoded_number += updated_num
+        print("Your password has been encoded and stored!")
+
+
+    elif option == "2":
+        print("The encoded password is " + encoded_number + "and the original password is " + choice + ".")
+        pass
+
+    elif option == "3":
+
+        break
+
+
+
+
+
